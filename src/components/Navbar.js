6f09df8579a5818d8d4e8bf63e36c5a572c505c2
Lam Bui Bao <lambuibao@outlook.com>
2022-03-28 21:53:46 +0700

    navbar updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,17 @@ import * as styles from './Navbar.modules.css'
 
 export default function Navbar() {
   return (
+    <>
     <header>
     <nav className="navbar" style={$} {...styles.navbar}>
-        <>
         <ul><Link to="/">Home</Link></ul>
         <ul><Link to="/about-us">About</Link></ul>
         <ul><Link to="/students">Students</Link></ul>
         <ul><Link to="/teachers">Teachers</Link></ul>
         <ul><Link to="/activities">Activities</Link></ul>
         <ul><Link to="/memories">Memories</Link></ul>
-        </>
     </nav>
     </header>
+    </>
   )
 }
