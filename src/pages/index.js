342671634560ff8530d49f21d37900c417bedd5c
Lam Bui Bao <lambuibao@outlook.com>
2022-03-28 22:05:10 +0700

    check for type of window

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,10 @@ import React from 'react'
 import Layout from '../templates/homepage/index.js'
 
 export default function Index() {
+  if (typeof window !== 'undefined') {
   return (
     <Layout />
   )
+  }
+  else return {}
 }
