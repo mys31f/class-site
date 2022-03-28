1cbabbd4499cb6b3b5df40073426d00a093133ea
Lam Bui Bao <lambuibao@outlook.com>
2022-03-28 21:50:45 +0700

    Update index.js

--- src/templates/homepage/index.js
+++ src/templates/homepage/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/Navbar.js'
+import Navbar from '../../components/Navbar.js'
 import homepageBG from '../../images/homepageBG.jpg'
 
 export default function Layout() {
