478a2147f9b364d537d424005136ad37dcc5ba03
Lam Bui Bao <lambuibao@outlook.com>
2022-03-31 22:31:25 +0700

    import fonts and navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import "@fontsource/montserrat/500.css" 
 
 export default function index() {
   return (
-    <div>index</div>
+    <>
+    <div className='navbar'>
+      <Navbar />
+    </div>
+    <div className='welcome'>
+      <h1 style={{
+          font-family: 'Montserrat', sans-serif;
+          font-size: '50px';
+          color: coral;
+          text-align: center;
+          text-shadow: 2px 2px black;
+        }}>
+          Welcome to our site.
+      </h1>
+    </div>
   )
 }
