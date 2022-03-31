1ac8cfd9245c45e7f47a9f14e45d1188a78ba086
Lam Bui Bao <lambuibao@outlook.com>
2022-03-31 23:31:01 +0700

    folders

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
-import '../index.module.css'
+import './index.module.css'
 
 export default function index() {
   return (
