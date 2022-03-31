429dc05267b5cc806ee48cc63573067afda45e0f
Lam Bui Bao <lambuibao@outlook.com>
2022-03-31 23:45:55 +0700

    navbar location

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import Navbar from '../components/Navbar/Navbar'
 import styles from  './index.module.css'
 
 export default function index() {
