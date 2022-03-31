00993c38fb43a90aabd1f7fe7219e43628017c28
Lam Bui Bao <lambuibao@outlook.com>
2022-03-31 22:54:01 +0700

    import styles from css cuz fuck inline

---
+++ src/pages/index/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import * as styles from '../styles/index.module.css'
+
+export default function index() {
+  return (
+    <>
+    <div className='navbar'>
+      <Navbar />
+    </div>
+    <div className='welcome' style={$} {...styles.welcome}>
+      <h1>
+          Welcome to our site.
+      </h1>
+    </div>
+    </>
+  )
+}
