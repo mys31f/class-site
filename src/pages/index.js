4e9b44ca3de3802334b71a6baedfcec6b5523746
Lam Bui Bao <lambuibao@outlook.com>
2022-04-02 21:27:54 +0700

    declare style for h1

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Navbar from '../components/Navbar/Navbar'
 import '@fontsource/montserrat/500.css'
 
 const styles = {
-  fontFamily: "Montserrat",
-  fontSize: "50px",
-  fontWeight: 500,
-  color: "coral",
-  textAlign: "center",
-  textShadow: "2px 2px black",
+  h1: {
+    fontFamily: "Montserrat",
+    fontSize: "50px",
+    fontWeight: 500,
+    color: "coral",
+    textAlign: "center",
+    textShadow: "2px 2px black",
+  }
 }
 
 export default function index() {
